5d9e307f92aeeaac3245a99f0293d72f57be43fa
ayushsj <ayushsj@iitk.ac.in>
2020-09-09 18:32:14 +0530

    HTTP part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -16,6 +16,7 @@ import { from } from 'rxjs';
 export class DishdetailComponent implements OnInit {
   
   dish: Dish;
+  errMess: string;
   dishcopy: Dish;
   dishIds: string[];
   prev: string;
@@ -55,7 +56,8 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-    .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); });
+    .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+    errmess => this.errMess = <any>errmess);
   }
 
   setPrevNext(dishId: string) {
